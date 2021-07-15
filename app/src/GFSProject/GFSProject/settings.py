f14be6b2aee84dd5ae4f9c7be2e3f6fd82fa10d4
Kona <kitami.kitano20@gmail.com>
2021-07-15 23:38:28 +0900

    db設定

--- app/src/GFSProject/GFSProject/settings.py
+++ app/src/GFSProject/GFSProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gfs.apps.GfsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'GFSProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'gfs-db',
+        'PORT': '3306'
     }
 }
 
