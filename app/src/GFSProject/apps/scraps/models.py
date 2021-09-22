1a2df3115c58a39a849d7ddf6625acd0be3e7141
jin237 <fgdaojebvoa237@gmail.com>
2021-09-22 21:46:06 +0900

    scraps_add

--- app/src/GFSProject/apps/scraps/models.py
+++ app/src/GFSProject/apps/scraps/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save
+import os.path
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):  # ...1
+        return self.name
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=20)
+    text = models.TextField(max_length=100, null=True, blank=True)
+    date = models.DateField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)  # ...2
+    tag = models.ManyToManyField(Tag, blank=True)  # ...3
+    del_flag = models.BooleanField(default=False)  # ...4
+    image = models.ImageField(blank=True, upload_to='event_pics')
+
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
--- app/src/GFSProject/apps/scraps/urls.py
+++ app/src/GFSProject/apps/scraps/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'scraps'
 
@@ -8,4 +10,9 @@ urlpatterns = [
     path('detail', views.ScrapsDetail.as_view(), name='scraps_detail'),
     path('edit', views.ScrapsEdit.as_view(), name='scraps_edit'),
     path('post', views.ScrapsPost.as_view(), name='scraps_post'),
+
+    path('', views.EventsList, name='index'),
+    path('post', views.PostEvent, name='event_post'),
+    path('detail/<int:id>/', views.EventDetail, name='event_detail'),
+
 ]
