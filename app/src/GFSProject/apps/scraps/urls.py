229e35eaed0dc976735b8ceb96399692ebbcce35
takehiro iino <50320445+jin237@users.noreply.github.com>
2021-09-22 22:43:32 +0900

    Update urls.py

--- app/src/GFSProject/apps/scraps/urls.py
+++ app/src/GFSProject/apps/scraps/urls.py
@@ -6,10 +6,10 @@ from django.conf import settings
 app_name = 'scraps'
 
 urlpatterns = [
-    path('list', views.ScrapsList.as_view(), name='scraps_list'),
-    path('detail', views.ScrapsDetail.as_view(), name='scraps_detail'),
-    path('edit', views.ScrapsEdit.as_view(), name='scraps_edit'),
-    path('post', views.ScrapsPost.as_view(), name='scraps_post'),
+    # path('list', views.ScrapsList.as_view(), name='scraps_list'),
+    # path('detail', views.ScrapsDetail.as_view(), name='scraps_detail'),
+    # path('edit', views.ScrapsEdit.as_view(), name='scraps_edit'),
+    # path('post', views.ScrapsPost.as_view(), name='scraps_post'),
 
     path('', views.EventsList, name='index'),
     path('post', views.PostEvent, name='event_post'),
